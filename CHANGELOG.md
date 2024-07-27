7e390c36dc952ec0c967beb38f5a5d9284a7c8a3
WhoSowSee <yanmishbash.i.s.2.21@gmail.com>
2024-07-27 22:10:43 +0300

    Bug fix update after repository initialization

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,13 @@ All notable changes to the "Git Exclude Watcher" extension will be documented in
 
 - Initial release of the Git Exclude Watcher extension.
 - Provides automatic Git status refresh for files affected by changes in `.git/info/exclude`.
+
+## [1.0.1] - <2024.07.27>
+
+### Fixed
+
+- Improved performance and readability of the code:
+  - Combined the initialization logic of watchers into a single function `checkAndSetupWatchers`.
+  - Simplified the check for the existence of the `.git` folder and the `exclude` file.
+  - Removed duplicate code fragments for better readability and easier maintenance.
+  - Optimized handling of file change events in `.git` and `.git/info/exclude`.
--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,78 @@
 import * as vscode from 'vscode'
 import * as path from 'path'
-import * as fs from 'fs/promises'
-import { watch } from 'fs'
-import debounce from 'lodash/debounce'
+import * as fs from 'fs'
 
 export function activate(context: vscode.ExtensionContext) {
 	const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath
 	if (!workspaceFolder) {
-		vscode.window.showErrorMessage('No workspace folder found')
 		return
 	}
 
-	const excludeFilePath = path.join(workspaceFolder, '.git', 'info', 'exclude')
+	const gitFolderPath = path.join(workspaceFolder, '.git')
+	const excludeFilePath = path.join(gitFolderPath, 'info', 'exclude')
 
-	fs.access(excludeFilePath)
-		.then(() => {
-			const watcher = watch(excludeFilePath)
+	const refreshGitStatus = async () => {
+		try {
+			await vscode.commands.executeCommand('git.refresh')
+		} catch (err) {
+			console.error(`Failed to refresh Git status: ${err}`)
+		}
+	}
 
-			const refreshGitStatus = debounce(async () => {
-				try {
-					await vscode.commands.executeCommand('git.refresh')
-				} catch (err) {
-					vscode.window.showErrorMessage(`Failed to refresh Git status: ${err}`)
+	const setupExcludeFileWatcher = () => {
+		if (fs.existsSync(excludeFilePath)) {
+			const watcher = fs.watch(excludeFilePath, (eventType, filename) => {
+				if (filename && eventType === 'change') {
+					refreshGitStatus()
 				}
-			}, 400)
-
-			watcher.on('change', refreshGitStatus)
+			})
 
 			context.subscriptions.push({
-				dispose: () => {
-					watcher.close()
-					refreshGitStatus.cancel()
-				},
+				dispose: () => watcher.close(),
 			})
+		}
+	}
+
+	const setupGitWatcher = () => {
+		const gitWatcher = vscode.workspace.createFileSystemWatcher(
+			new vscode.RelativePattern(workspaceFolder, '.git/**')
+		)
+
+		gitWatcher.onDidCreate(uri => {
+			if (uri.fsPath === excludeFilePath) {
+				setupExcludeFileWatcher()
+			}
 		})
-		.catch(() => {
-			vscode.window.showErrorMessage(`File ${excludeFilePath} does not exist`)
+
+		gitWatcher.onDidChange(uri => {
+			if (uri.fsPath === excludeFilePath) {
+				refreshGitStatus()
+			}
 		})
+
+		context.subscriptions.push(gitWatcher)
+	}
+
+	const checkAndSetupWatchers = () => {
+		if (fs.existsSync(gitFolderPath)) {
+			setupGitWatcher()
+			setupExcludeFileWatcher()
+		} else {
+			const checkGitFolderExistence = setInterval(() => {
+				if (fs.existsSync(gitFolderPath)) {
+					clearInterval(checkGitFolderExistence)
+					setupGitWatcher()
+					setupExcludeFileWatcher()
+				}
+			}, 1000)
+
+			context.subscriptions.push({
+				dispose: () => clearInterval(checkGitFolderExistence),
+			})
+		}
+	}
+
+	checkAndSetupWatchers()
 }
 
 export function deactivate() {}
